39f10590d799dc1c91919e1dfa6bb84047c198b5
youssef shaaban <ysfme2@hotmail.com>
2023-10-15 21:29:24 +0300

    send to serveral emails at the same time and cashing settings

--- EmailSender/MainPage.xaml.cs
+++ EmailSender/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EmailSender
 {
@@ -11,6 +13,42 @@ namespace EmailSender
         {
             InitializeComponent();
             btnSend.Clicked += BtnSend_Clicked;
+            this.Appearing += MainPage_Appearing;
+            this.Disappearing += MainPage_Disappearing;
+        }
+
+        private async void MainPage_Disappearing(object sender, EventArgs e)
+        {
+            try
+            {
+                await SecureStorage.SetAsync("Email", txtEmail.Text);
+                await SecureStorage.SetAsync("Name", txtName.Text);
+                await SecureStorage.SetAsync("Password", txtPassword.Text);
+                await SecureStorage.SetAsync("SMTP", txtsmtp.Text);
+                await SecureStorage.SetAsync("Port", txtPort.Text);
+                await SecureStorage.SetAsync("Subject", txtSubject.Text);
+                await SecureStorage.SetAsync("Message", txtMessage.Text);
+                await SecureStorage.SetAsync("Emails", txtEmails.Text);
+            }
+            catch (Exception ex) { }
+        }
+
+        private async void MainPage_Appearing(object sender, EventArgs e)
+        {
+            try
+            {
+                txtEmail.Text = await SecureStorage.Default.GetAsync("Email") ?? "";
+                txtName.Text = await SecureStorage.Default.GetAsync("Name") ?? "";
+                txtPassword.Text = await SecureStorage.Default.GetAsync("Password") ?? "";
+                txtsmtp.Text = await SecureStorage.Default.GetAsync("SMTP") ?? txtsmtp.Text;
+                txtPort.Text = await SecureStorage.Default.GetAsync("Port") ?? txtPort.Text;
+                txtSubject.Text = await SecureStorage.Default.GetAsync("Subject") ?? "";
+                txtMessage.Text = await SecureStorage.Default.GetAsync("Message") ?? "";
+                txtEmails.Text = await SecureStorage.Default.GetAsync("Emails") ?? "";
+            }
+            catch (Exception ex)
+            {
+            }
         }
 
         private async void BtnSend_Clicked(object sender, EventArgs e)
@@ -22,50 +60,50 @@ namespace EmailSender
                 Disable_EnableControls(true);
                 return;
             }
-            int interval = 1;
-            if (!int.TryParse(txtIntervals.Text, out interval))
+
+            var emails = txtEmails.Text.Trim().Split("\r").ToList().Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x));
+
+            var toEmail = string.Join(",", emails);
+            var resumeFileStream = await resumeFile.OpenReadAsync();
+            try
             {
-                await DisplayAlert("Invalid information", "please input valid delay interval in seconds!", "Ok");
-                Disable_EnableControls(true);
-                return;
+                await Task.Run(() => SendToMail(toEmail,new Attachment(resumeFileStream,"Youssef Shaaban Resume.pdf")));
+
+                await DisplayAlert("Sent", "Your Email Sent successfully!", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message + "\n\n\n" + ex.InnerException?.Message, "Ok");
             }
 
-            string[] toEmails = txtEmails.Text.Trim().Split('\n');
-            foreach (string toEmail in toEmails)
-            {
-                await SendToMail(toEmail);
-
-                await Task.Delay(interval * 1000);
-            }
             Disable_EnableControls(true);
         }
 
-        private async Task SendToMail(string toEmail)
+        private void SendToMail(string toEmail, params Attachment[] attachments)
         {
+
             MailMessage mailMessage = new MailMessage(txtEmail.Text, toEmail);
             mailMessage.Subject = txtSubject.Text;
-            mailMessage.Sender = new MailAddress(txtSubject.Text, txtName.Text);
+            mailMessage.Sender = new MailAddress(txtEmail.Text, txtName.Text);
             mailMessage.Body = txtMessage.Text;
+            foreach (var attachment in attachments)
+            {
+                mailMessage.Attachments.Add(attachment);
+            }
 
             SmtpClient smtpClient = new SmtpClient(txtsmtp.Text, int.Parse(txtPort.Text));
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.UseDefaultCredentials = false;
             smtpClient.EnableSsl = true;
             smtpClient.Credentials = new NetworkCredential(txtEmail.Text.Trim(), txtPassword.Text.Trim());
-            try
-            {
-                await smtpClient.SendMailAsync(mailMessage);
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error", ex.Message, "Ok");
-            }
+
+            smtpClient.Send(mailMessage);
         }
 
         private void Disable_EnableControls(bool enable)
         {
-            txtEmail.IsEnabled = txtEmails.IsEnabled = txtIntervals.IsEnabled = txtMessage.IsEnabled
-                = txtPassword.IsEnabled = txtPort.IsEnabled = txtResume.IsEnabled =
+            txtEmail.IsEnabled = txtEmails.IsEnabled = txtMessage.IsEnabled
+                = txtPassword.IsEnabled = txtPort.IsEnabled = txtResume.IsEnabled = txtName.IsEnabled = txtsmtp.IsEnabled =
                 txtSubject.IsEnabled = btnSend.IsEnabled = btnLoadResume.IsEnabled = enable;
         }
 
